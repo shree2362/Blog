197592d9d1e1a5244254541b0d1cd6114312f429
CodeCraver <72202629+shree2362@users.noreply.github.com>
2023-09-15 01:07:21 +0530

    Update posts.sql

--- posts.sql
+++ posts.sql
@@ -1,11 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Sep 13, 2023 at 05:40 PM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.0.28
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
